dcae5005ee5fd0e4726ec3842b1bcc06556ac8b6
Felix Martinez <felixaviermartinez@gmail.com>
2019-08-18 16:17:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) 
+  array.each_with_index { |item, index|
+  puts "#{index + 1} #{item}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  captain_planet = []
+  array.each { |element| captain_planet.push("#{element.capitalize}!") }
+  captain_planet
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |e|
+    if e.length > 4
+      return true
+    else
+    end
+  end
+  return false
 end
 
 def find_the_cheese# code an argument here
